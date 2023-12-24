617027079a5ee97e39eb3d8c90b90afb38b7b28e
PlumJE <154695242+PlumJE@users.noreply.github.com>
2023-12-24 09:47:57 +0900

    first commit
    Add first 6 folders

---
+++ Hello_World/main.py
@@ -0,0 +1,42 @@
+from kivy.properties import ColorProperty
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.button import MDRectangleFlatButton
+
+#메인 스크린 안에 있는 4*4 그리드 레이아웃
+class SubGrid(MDGridLayout):
+    def __init__(self, **kwargs):
+        super(SubGrid, self).__init__(**kwargs)
+        self.cols = 2
+
+        self.add_widget(MDLabel(text='Kivy parrot : ', halign='right'))#1행 1열에 "Kivy parrot : "이란 라벨
+        self.kivy_output = MDLabel(text='Hello, world!', outline_color=ColorProperty())#2행 1열에 "Hello, world!"가 디폴트인 라벨
+        print(type(self.kivy_output.outline_color))
+        self.add_widget(self.kivy_output)
+
+        self.add_widget(MDLabel(text='You : ', halign='right')) #1행 2열에 "You : "이란 라벨
+        self.user_input = MDTextField()                         #2행 2열에 사용자의 입력을 받는 텍스트필드
+        self.add_widget(self.user_input)
+
+#앱에 먼저 등장할 메인 스크린
+class MainScreen(MDGridLayout):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        self.cols = 1
+        self.subGrid = SubGrid(size_hint_y=0.8) #1행 1열에 4*4 그리드 레이아웃
+        self.add_widget(self.subGrid)
+
+        self.echo_button = MDRectangleFlatButton(text='Talk to Kivy!', on_press=self.echo_func) #사용자의 입력을 Kivy앵무새가 따라하게 하는 버튼
+        self.add_widget(self.echo_button)       #1행 2열에 버튼을 추가
+    #사용자의 입력을 2행 1열의 라벨에 붙여넣는 동작
+    def echo_func(self, *args):
+        self.subGrid.kivy_output.text = self.subGrid.user_input.text
+
+#앱 전체를 상징하는 클래스
+class HelloWorldApp(MDApp):
+    def build(self):
+        return MainScreen(pos_hint={'center_y':0.5}, size_hint_y=0.5)
+if __name__ == "__main__":
+    HelloWorldApp().run()
---
+++ My_Paint/main.py
@@ -0,0 +1,34 @@
+from random import random
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+
+
+#앱 속에서 실행될 그림판 위젯
+class MyPaintWidget(Widget):
+    def on_touch_down(self, touch):
+        color = (random(), 1, 1)
+        with self.canvas:
+            Color(*color, mode='hsv')
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+
+#앱 전체를 상징하는 클래스
+class MyPaintApp(App):
+    def build(self):
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        #버튼 위젯과 그림판 위젯을 서로 합친다
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
+if __name__ == '__main__':
+    MyPaintApp().run()
